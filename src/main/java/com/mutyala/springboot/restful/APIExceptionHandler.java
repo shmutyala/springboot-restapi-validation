a2cfe4a65db39419400de5a83f67b4c1ee49dd2b
shrikant_k_mutyala <shrikantmutyala@gmail.com>
2020-05-23 20:00:58 +0530

    Validation

---
+++ src/main/java/com/mutyala/springboot/restful/APIExceptionHandler.java
@@ -0,0 +1,39 @@
+package com.mutyala.springboot.restful;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import javax.validation.ConstraintViolationException;
+
+@Slf4j
+@ControllerAdvice
+public class APIExceptionHandler extends ResponseEntityExceptionHandler {
+    @Override
+    protected ResponseEntity handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+        FieldError fieldError = ex.getBindingResult().getFieldError();
+        ResponseDTO responseDTO = ResponseDTO.builder()
+            .status(status.toString())
+            .message(fieldError.getDefaultMessage()).build();
+
+        return ResponseEntity.badRequest().body(responseDTO);
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public final ResponseEntity<Object> handleConstraintViolationException(Exception ex, WebRequest request) {
+
+        ResponseDTO responseDTO = ResponseDTO.builder()
+            .status(HttpStatus.BAD_REQUEST.toString())
+            .message(ex.getMessage()).build();
+
+        return ResponseEntity.badRequest().body(responseDTO);
+    }
+}
